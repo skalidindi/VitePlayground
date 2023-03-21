e8501e23a1b7dcf4cf3ce465c9bb2a1116e263a8
Santosh Kalidindi <skalidindi@netflix.com>
2023-03-21 15:50:52 -0700

    monaco

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import eslintPlugin from "vite-plugin-eslint";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin({ fix: true })],
+  plugins: [
+    react(),
+    eslintPlugin({ fix: true }),
+    monacoEditorPlugin({ globalAPI: true }),
+  ],
 });
